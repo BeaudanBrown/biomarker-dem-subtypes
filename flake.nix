938dda2f4f648f9256516d34b278f35bc107c8c6
Beaudan Brown <beaudan.brown@gmail.com>
2025-05-11 13:05:48 +1000

    Add pre commit hooks

--- flake.nix
+++ flake.nix
@@ -1,19 +1,37 @@
 {
   description = "A basic flake with a shell";
-  outputs = { nixpkgs, flake-utils, ... }:
+  outputs = { self, nixpkgs, flake-utils, pre-commit-hooks, ... }:
     flake-utils.lib.eachDefaultSystem (
       system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
       in
         {
+        checks = {
+          pre-commit-check = pre-commit-hooks.lib.${system}.run {
+            src = ./.;
+            hooks = {
+              air-fmt = {
+                enable = true;
+                entry = "air format";
+                files = ".*\.[rR]$";
+              };
+            };
+          };
+        };
+
         devShells.default = pkgs.mkShell {
+          inherit (self.checks.${system}.pre-commit-check) shellHook;
           env.R_LIBS_USER="./.Rlib";
-          buildInputs = [ pkgs.bashInteractive ];
+          buildInputs = [
+            pkgs.bashInteractive
+            self.checks.${system}.pre-commit-check.enabledPackages
+          ];
           packages = with pkgs;
             [
               R
               quarto
+              air-formatter
             ] ++ (with rPackages; [
               languageserver
               dotenv
@@ -66,6 +84,7 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     systems.url = "github:nix-systems/default";
+    pre-commit-hooks.url = "github:cachix/git-hooks.nix";
     flake-utils = {
       url = "github:numtide/flake-utils";
       inputs.systems.follows = "systems";
